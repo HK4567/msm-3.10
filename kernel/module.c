730021fe980eba63a0281dd4f17cc26e932081bb
Christopher N. Hesse <raymanfx@gmail.com>
2017-07-20 01:58:26 +0200

    kernel: module: Ignore all magic mismatches
    Change-Id: I3b9012574aa0069131cfd32aecdefa6b00b907bb

--- kernel/module.c
+++ kernel/module.c
@@ -3209,6 +3209,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	struct module *mod;
 	long err;
 
+	//FIXME
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+
 	err = module_sig_check(info);
 	if (err)
 		goto free_copy;
